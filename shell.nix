cd40a0b9b3d90506f92b209b51763cbdb23baf80
Sophie Taylor <sophie@spacekitteh.moe>
2016-11-05 16:13:04 +1000

    bleh

--- shell.nix
+++ shell.nix
@@ -2,12 +2,13 @@
 let
   inherit (nixpkgs) pkgs;
   ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; 
-[ ghc-mod mtl lens parsec QuickCheck ansi-wl-pprint text text-format annotated-wl-pprint cabal-install
-alex zippers compdata
-happy monad-logger hlint unordered-containers ekg haskeline 
+[ ghc-mod mtl lens parsec QuickCheck ansi-wl-pprint text text-format annotated-wl-pprint cabal-install mainland-pretty trifecta
+alex zippers compdata fgl doctest hslogger 
+happy monad-logger hlint unordered-containers ekg haskeline #ivory 
 smartcheck sbv tasty optparse-applicative configurator 
 leksah data-category
 #megaparsec
+idris
 structured-haskell-mode]);
   texlive = pkgs.texLiveFull;
    
@@ -22,7 +23,9 @@ structured-haskell-mode]);
             company-ghc
             flycheck-haskell
             org-plus-contrib
+            idris-mode
             org-trello            
+            auctex
             magit ]);
   llvm = pkgs.llvm_35;
   polly = pkgs.llvmPackages_35.polly;
@@ -30,8 +33,10 @@ structured-haskell-mode]);
   z3 = pkgs.z3;
   cabal2nix = pkgs.cabal2nix;
   coq = pkgs.coq_8_5;
+  isabelle = pkgs.isabelle;
   coqPackages = pkgs.coqPackages_8_5;
   busybox = pkgs.busybox;
+  sphinx = pkgs.pythonPackages.sphinx;
   openssl = pkgs.openssl;
 in
 pkgs.stdenv.mkDerivation {
@@ -39,10 +44,12 @@ pkgs.stdenv.mkDerivation {
   buildInputs = [ghc
   texlive
   emacsWithMyPackages
+  sphinx
   llvm
   polly
   git
   z3
+  isabelle
   cabal2nix
   busybox
   openssl
